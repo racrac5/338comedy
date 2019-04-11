5f539020af8bc8eb2d2e17bbbc4794e2cbdb34e8
Jair Rojas <Jair>
2019-04-11 15:52:17 -0500

    form input sends to python file

--- cgi-bin/parser.py
+++ cgi-bin/parser.py
@@ -2,12 +2,11 @@
 
 import cgi, cgitb
 
+form = cgi.FieldStorage()
 first_name = form.getvalue('first_name')
 last_name  = form.getvalue('last_name')
+
 print("Content-type:text/html")
-print("")
-print("")
-print("Hello - Second CGI Program")
 print("")
 print("")
 print("   Hello %s %s" % (first_name, last_name))

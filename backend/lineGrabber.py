a0055e11cbc97dae720be1b74bc5f0ebe53aa8f5
Ryan Callaghan <39535195+racrac5@users.noreply.github.com>
2019-04-25 17:14:53 -0500

    uptodate linegrabber

--- backend/lineGrabber.py
+++ backend/lineGrabber.py
@@ -14,17 +14,20 @@ def main():
     #file_list = glob.glob(os.path.join(os.getcwd(), "sketches txt files", "*.txt")) ;
 
     myfile = open("/Users/ryancallaghan/Desktop/EECS/338/338comedy/sketches txt files/soccercommentating.txt")
+    # not necessary to seach whole folder now, they are working on tags as object feature
     lines = myfile.readlines()
 
     for line in lines:
         index = index+1;
-        if "soccer" in line:
+        if "Massive" in line:
             relevance = relevance + 1;
         if (relevance > max):
             printlin = index;
         relevance = 0;
-    print(lines[printlin]);
-    return lines[printlin] ;
+
+    if printlin != -1:
+        print(lines[printlin]);
+        return lines[printlin] ;
 
 main();
 

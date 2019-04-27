2c49c0d5abcb2bbbb07276399bad3837fb68459e
Jair Rojas <jairrojas2020@u.northwestern.edu>
2019-04-26 23:07:27 -0500

    cleaned up code and gave myfile a relative path

--- backend/lineGrabber.py
+++ backend/lineGrabber.py
@@ -1,35 +1,25 @@
-import nltk;
-import glob;
-import os;
-#import sketches_txt_files/soccercommentating;
+import nltk
 
 def main():
-    searchword = "soccer";
-    corpus = [] ;
-    relevance = 0;
-    max = 0;
-    index = -1;
-    printlin = 0;
+    searchword = "soccer"
+    corpus = []
+    relevance = 0
+    max_amount = 0
+    index = -1
+    printlin = 0
 
-    #file_list = glob.glob(os.path.join(os.getcwd(), "sketches txt files", "*.txt")) ;
-
-    myfile = open("/Users/ryancallaghan/Desktop/EECS/338/338comedy/sketches txt files/soccercommentating.txt")
     # not necessary to seach whole folder now, they are working on tags as object feature
+    myfile = open("../scripting/sketches txt files/soccercommentating.txt", encoding="utf8")
     lines = myfile.readlines()
 
     for line in lines:
-        index = index+1;
-        if "Massive" in line:
-            relevance = relevance + 1;
-        if (relevance > max):
-            printlin = index;
-        relevance = 0;
+        index += 1
+        if "massive" in line.lower(): relevance += 1
+        if (relevance > max_amount): printlin = index
+        relevance = 0
 
     if printlin != -1:
-        print(lines[printlin]);
-        return lines[printlin] ;
+        print(lines[printlin])
+        # return lines[printlin]
 
-main();
-
-#    with open(file_path) as f_input:
-#        corpus.append(f_input.read())
+main()
